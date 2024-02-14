c975eabb10d567c39fe8104883d15d0cbb9aa1a0
Rhodiumtech <support@rhodium-tech.com>
2024-02-14 14:24:12 +0500

    Initial commit

--- lib/partyscreen.dart
+++ lib/partyscreen.dart
@@ -9,7 +9,7 @@ class PartyScreen extends StatelessWidget {
           children: [
             Container(
               width: 390,
-              height: 500, // Increased height to accommodate text and image
+              height: 500,
               clipBehavior: Clip.antiAlias,
               decoration: ShapeDecoration(
                 color: Colors.white,
@@ -110,16 +110,16 @@ class PartyScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 10), // Adjust the gap between text and image
+                  SizedBox(height: 10),
                   Container(
                     width: 229,
                     height: 229,
                     margin: const EdgeInsets.only(
-                        bottom: 10), // Added margin for spacing
+                        bottom: 10),
                     decoration: BoxDecoration(
                       image: DecorationImage(
                         image: AssetImage(
-                            "assets/objects.png"), // Replace with your image path
+                            "assets/objects.png"),
                         fit: BoxFit.cover,
                       ),
                       borderRadius: BorderRadius.circular(15),
